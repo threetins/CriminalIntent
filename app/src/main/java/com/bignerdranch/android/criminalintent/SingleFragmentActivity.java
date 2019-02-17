d174c83e1a3199e69afb6d24e414a0c2552fb587
threetins <peternuell@hotmail.com>
2019-02-17 09:02:05 +0000

    abstract class fragment activity Done

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,10 +1,10 @@
+package com.bignerdranch.android.criminalintent;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-
-import com.bignerdranch.android.criminalintent.R;
 
 public abstract class SingleFragmentActivity extends AppCompatActivity {
 
@@ -24,4 +24,3 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
         }
     }
 }
-
