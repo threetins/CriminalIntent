e5e806a3f9f31508989194a2326f539bbd83f314
threetins <peternuell@hotmail.com>
2019-02-22 11:49:08 +0000

    up till ch10 challenge

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -11,7 +11,8 @@ import java.util.UUID;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
-    public static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
     public static Intent newIntent(Context packageContext, UUID crimeId) {
         Intent intent = new Intent(packageContext, CrimeActivity.class);
         intent.putExtra(EXTRA_CRIME_ID, crimeId);
@@ -20,6 +21,7 @@ public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        return CrimeFragment.newInstance(crimeId);
     }
 }
